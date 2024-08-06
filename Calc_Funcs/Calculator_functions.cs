2b0e0ac2d1eb31ced51a7a49637718aac98301bc
MeVickeyKr <vk7743593@gmail.com>
2024-08-06 17:56:26 +0530

    FIRST COMMIT

---
+++ Calc_Funcs/Calculator_functions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+namespace Calc_Funcs
+{
+    public class Perform_Calc
+    {
+
+        public static dynamic Perform_Add(dynamic obj1, dynamic obj2)
+        {
+            try
+            {
+                dynamic d1 = ConvertTONUmeric(obj1);
+                dynamic d2 = ConvertTONUmeric(obj2);
+                return d1 + d2;
+            }
+            catch (Exception )
+            {
+                return "not a valid type";
+            }
+        }
+        public static dynamic Perform_Sub(dynamic obj1, dynamic obj2)
+        {
+            try
+            {
+                dynamic d1 = ConvertTONUmeric(obj1);
+                dynamic d2 = ConvertTONUmeric(obj2);
+                return d1 - d2;
+            }
+            catch (Exception )
+            {
+                return "not a valid type";
+            }
+
+        }
+        public static dynamic Perform_Mul(dynamic obj1, dynamic obj2)
+        {
+            try
+            {
+                dynamic d1 = ConvertTONUmeric(obj1);
+                dynamic d2 = ConvertTONUmeric(obj2);
+                return d1 * d2;
+            }
+            catch (Exception )
+            {
+                return "not a valid type";
+            }
+        }
+        public static dynamic Perform_Div(dynamic obj1, dynamic obj2)
+        {
+            try
+            {
+                dynamic d1 = ConvertTONUmeric(obj1);
+                dynamic d2 = ConvertTONUmeric(obj2);
+
+                return d1 / d2;
+            }
+            catch (Exception)
+            {
+                return "not a valid type";
+            }
+        }
+        private static dynamic ConvertTONUmeric(dynamic value)
+        {
+            if(value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+            if(value is string s) 
+            {
+                return double.Parse(s);
+            }
+            if(value is IConvertible)
+            {
+                return Convert.ChangeType(value, typeof(double));
+            }
+            return "invalid type";
+        }
+    }
+}
---
+++ calculator/Calculator_main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Calc_Funcs;
+
+namespace Claculator
+{
+    internal class Calculator_main
+    {
+        static void Main(string[] args)
+        {
+            dynamic result1 =Perform_Calc.Perform_Add(7, 7);
+            dynamic result2 = Perform_Calc.Perform_Add("9", 3.5);
+            dynamic result3 = Perform_Calc.Perform_Add("rohan", "ankit");
+            dynamic result5 = Perform_Calc.Perform_Add("" , 12);
+            Console.WriteLine(result5);
+            Console.WriteLine(result3);
+           Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
+           dynamic result4 = Perform_Calc.Perform_Sub(7, 7);
+            Console.WriteLine(result4);
+
+
+            Console.ReadKey();
+        }
+    }
+}
